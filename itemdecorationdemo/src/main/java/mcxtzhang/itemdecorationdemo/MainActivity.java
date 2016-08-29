114539b45aa4f164a6f5250b90e9960dcc58e687
zhangxutong <zhangxutong@imcoming.cn>
2016-08-29 15:08:24 +0800

    加入sideBar和联动滑动

--- itemdecorationdemo/src/main/java/mcxtzhang/itemdecorationdemo/MainActivity.java
+++ itemdecorationdemo/src/main/java/mcxtzhang/itemdecorationdemo/MainActivity.java
@@ -4,12 +4,15 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import mcxtzhang.itemdecorationdemo.decoration.DividerItemDecoration;
 import mcxtzhang.itemdecorationdemo.decoration.TitleItemDecoration;
+import mcxtzhang.itemdecorationdemo.widget.SideBar;
 
 public class MainActivity extends Activity {
     private static final String TAG = "zxt";
@@ -19,6 +22,18 @@ public class MainActivity extends Activity {
     private List<CityBean> mDatas;
 
     private TitleItemDecoration mDecoration;
+
+
+    /**
+     * 右侧边栏导航区域
+     */
+    private SideBar mSideBar;
+
+    /**
+     * 显示指示器DialogText
+     */
+    private TextView mTvSideBarHint;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +47,20 @@ public class MainActivity extends Activity {
         //如果add两个，那么按照先后顺序，依次渲染。
         //mRv.addItemDecoration(new TitleItemDecoration2(this,mDatas));
         mRv.addItemDecoration(new DividerItemDecoration(MainActivity.this, DividerItemDecoration.VERTICAL_LIST));
+
+        mSideBar = (SideBar) findViewById(R.id.sideBar);
+        mTvSideBarHint = (TextView) findViewById(R.id.tvSideBarHint);
+        mSideBar.setTextView(mTvSideBarHint);
+        mSideBar.setOnTouchingLetterChangedListener(new SideBar.OnTouchingLetterChangedListener() {
+            @Override
+            public void onTouchingLetterChanged(String s) {
+                int position = getPosByTag(s);
+                if (position != -1) {
+                    mManager.scrollToPositionWithOffset(position/* + mAdapter.getHeaderViewsCount()*/, 0);
+                }
+            }
+        });
+
     }
 
     private void initDatas() {
@@ -48,5 +77,23 @@ public class MainActivity extends Activity {
         mDatas.add(new CityBean("H", "湖南"));
         mDatas.add(new CityBean("H", "河北"));
         mDatas.add(new CityBean("J", "江苏"));
+    }
+
+
+    /**
+     * 根据传入的pos返回tag
+     * @param tag
+     * @return
+     */
+    private int getPosByTag(String tag) {
+        if (TextUtils.isEmpty(tag)) {
+            return -1;
+        }
+        for (int i = 0; i < mDatas.size(); i++) {
+            if (tag.equals(mDatas.get(i).getTag())) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
