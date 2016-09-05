39a2e93492766fa9dfa6a50d817cf3a2d2fb144a
zhangxutong <zhangxutong@imcoming.cn>
2016-09-05 22:18:50 +0800

    暂存公司

---
+++ cstviewdemo/src/main/java/com/mcxtzhang/cstviewdemo/widget/CanvasView.java
@@ -0,0 +1,58 @@
+package com.mcxtzhang.cstviewdemo.widget;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.mcxtzhang.cstviewdemo.R;
+
+/**
+ * 介绍：自定义View 主要练习Canvas Paint
+ * 作者：zhangxutong
+ * 邮箱：zhangxutong@imcoming.com
+ * 时间： 2016/9/1.
+ */
+public class CanvasView extends View {
+
+    private Paint mPaint;
+    private Bitmap mBitmap;
+
+    public CanvasView(Context context) {
+        this(context, null);
+    }
+
+    public CanvasView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CanvasView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(Context context) {
+        mPaint = new Paint();
+        mPaint.setTextSize(30);
+        mPaint.setAntiAlias(true);
+        mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.wangcong);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        setBackgroundColor(Color.GREEN);
+        mPaint.setColor(Color.YELLOW);
+        canvas.rotate(90);
+
+        canvas.drawRect(0, -getWidth(), getHeight(), 0, mPaint);
+
+        canvas.drawBitmap(mBitmap,0,-mBitmap.getHeight(),mPaint);
+
+        mPaint.setColor(Color.RED);
+        canvas.drawText("123456789", 50, -50, mPaint);
+    }
+}
--- lib/src/main/java/com/example/MyClass.java
+++ lib/src/main/java/com/example/MyClass.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class MyClass {
     private static final int MODE_SHIFT = 30;
-    private static final int MODE_MASK  = 0x3 << MODE_SHIFT;
+    private static final int MODE_MASK = 0x3 << MODE_SHIFT;
 
     /**
      * Measure specification mode: The parent has not imposed any constraint
@@ -15,13 +15,14 @@ public class MyClass {
      * for the child. The child is going to be given those bounds regardless
      * of how big it wants to be.
      */
-    public static final int EXACTLY     = 1 << MODE_SHIFT;
+    public static final int EXACTLY = 1 << MODE_SHIFT;
 
     /**
      * Measure specification mode: The child can be as large as it wants up
      * to the specified size.
      */
-    public static final int AT_MOST     = 2 << MODE_SHIFT;
+    public static final int AT_MOST = 2 << MODE_SHIFT;
+
     public static void main(String[] args) {
         System.out.println(MODE_MASK);
         System.out.println(UNSPECIFIED);
@@ -29,7 +30,18 @@ public class MyClass {
         System.out.println(AT_MOST);
 
 
-        System.out.println(0+1234);
-        System.out.println(0|1234);
+        System.out.println(0 + 1234);
+        System.out.println(0 | 1234);
+
+
+        Integer a = 1;
+        Integer b = 1;
+
+        Integer c = new Integer(1);
+        Integer d = new Integer(1);
+
+
+        System.out.println((a==b)+"????");
+        System.out.println((c.equals(d))+"????");
     }
 }
