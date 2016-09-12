4d14b9d94c45bcc4a5823331d5a5d0cef595efe2
zhangxutong <zhangxutong@imcoming.cn>
2016-09-12 22:35:57 +0800

    MediaDemo

---
+++ mediademo/src/main/java/mcxtzhang/mediademo/TookVideoActivity.java
@@ -0,0 +1,44 @@
+package mcxtzhang.mediademo;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.VideoView;
+
+public class TookVideoActivity extends AppCompatActivity {
+    private VideoView mVideoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_took_video);
+        mVideoView = (VideoView) findViewById(R.id.mVideoView);
+    }
+
+
+    public void tookVideo(View view) {
+        dispatchTakeVideoIntent();
+    }
+
+    static final int REQUEST_VIDEO_CAPTURE = 1;
+
+    private void dispatchTakeVideoIntent() {
+        Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
+            Uri videoUri = data.getData();
+            Uri uri = Uri.parse("rtsp://v2.cache2.c.youtube.com/CjgLENy73wIaLwm3JbT_%ED%AF%80%ED%B0%819HqWohMYESARFEIJbXYtZ29vZ2xlSARSB3Jlc3VsdHNg_vSmsbeSyd5JDA==/0/0/0/video.3gp");
+            mVideoView.setVideoURI(videoUri);
+            mVideoView.start();
+        }
+    }
+}
