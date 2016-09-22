d44558dc087281d9111cdabe140f640e25dee9ad
zhangxutong <zhangxutong@imcoming.cn>
2016-09-22 14:28:22 +0800

    String升

--- lib/src/main/java/com/example/ZhengZeClass.java
+++ lib/src/main/java/com/example/ZhengZeClass.java
@@ -1,10 +1,5 @@
 package com.example;
 
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 /**
  * 介绍：
  * 作者：zhangxutong
@@ -16,7 +11,7 @@ public class ZhengZeClass {
 
 
     public static void main(String[] arg) {
-        AtomicBoolean aBoolean = new AtomicBoolean(false);
+/*        AtomicBoolean aBoolean = new AtomicBoolean(false);
         System.out.println("before aBoolean:"+aBoolean);
         if (aBoolean.compareAndSet(false,true)){
             System.out.println("if aBoolean:"+aBoolean);
@@ -40,6 +35,11 @@ public class ZhengZeClass {
         Pattern regex1 = Pattern.compile("^(17[0-9]|13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\\d{8}$");
         Matcher matcher = regex1.matcher("186163203845");
         boolean isMatched = matcher.matches();
-        System.out.print("isMatched:"+isMatched);
+        System.out.print("isMatched:"+isMatched);*/
+
+        String s1= new String("上海");
+        String s2= new String("a");
+        System.out.print("s1:"+s1/*.toUpperCase()*/);
+        System.out.print("s2:"+s2.toUpperCase());
     }
 }
