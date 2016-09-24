04ab2c693c5bd565615f93ab7c792a4abbf0e61f
zhangxutong <zhangxutong@imcoming.cn>
2016-09-24 21:21:21 +0800

    Db & NestRv

---
+++ databindingdemo/src/main/java/com/mcxtzhang/databindingdemo/FragmentActivity.java
@@ -0,0 +1,17 @@
+package com.mcxtzhang.databindingdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FragmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+        getSupportFragmentManager().
+                beginTransaction().
+                replace(R.id.activity_fragment, new BlankFragment()).
+                commitAllowingStateLoss();
+    }
+}
