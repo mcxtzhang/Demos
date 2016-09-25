55baf969a3ac5759a7f17f4783d3517c0443746d
zhangxutong <zhangxutong@imcoming.cn>
2016-09-25 23:40:54 +0800

    实验链式

---
+++ app/src/main/java/com/mcxtzhang/github/bean/TestLineBean.java
@@ -0,0 +1,15 @@
+package com.mcxtzhang.github.bean;
+
+/**
+ * 介绍：
+ * 作者：zhangxutong
+ * 邮箱：mcxtzhang@163.com
+ * CSDN：http://blog.csdn.net/zxt0601
+ * 时间： 16/09/25.
+ */
+
+public class TestLineBean {
+    private String name;
+    private String url;
+
+}
