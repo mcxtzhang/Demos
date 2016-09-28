08a746a08945dee63c1ba3d9dd8e971c1880eed1
zhangxutong <zhangxutong@imcoming.cn>
2016-09-28 17:42:01 +0800

    BASE64 DEMO

--- app/src/main/java/com/mcxtzhang/github/TestNewASActivity.java
+++ app/src/main/java/com/mcxtzhang/github/TestNewASActivity.java
@@ -1,7 +1,12 @@
 package com.mcxtzhang.github;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class TestNewASActivity extends AppCompatActivity {
 
@@ -9,5 +14,60 @@ public class TestNewASActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_new_as);
+
+        //http://www.cnblogs.com/whoislcj/p/5887859.html
+
+/*        String encodedString = Base64.encodeToString("whoislcj".getBytes(), Base64.DEFAULT);
+        Log.e("Base64", "Base64---->" + encodedString);
+        String decodedString = new String(Base64.decode(encodedString, Base64.DEFAULT));
+        Log.e("Base64", "Base64---->" + decodedString);*/
+
+
+/*        File file = new File("file:///android_asset/cc.png");
+        FileInputStream inputFile = null;
+        try {
+            inputFile = new FileInputStream(file);
+            byte[] buffer = new byte[(int) file.length()];
+            inputFile.read(buffer);
+            inputFile.close();
+            String encodedString = Base64.encodeToString(buffer, Base64.DEFAULT);
+            Log.e("Base64", "Base64---->" + encodedString);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
+
+
+/*        File desFile = new File("/storage/emulated/0/pimsecure_debug_1.txt");
+        FileOutputStream fos = null;
+        try {
+            byte[] decodeBytes = Base64.decode(encodedString.getBytes(), Base64.DEFAULT);
+            fos = new FileOutputStream(desFile);
+            fos.write(decodeBytes);
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
+
+
+        Log.i("TAG", Base64.encodeToString(getFromAssets("cc.png").getBytes(), Base64.DEFAULT));
+
     }
+
+
+    public String getFromAssets(String fileName) {
+        try {
+            InputStreamReader inputReader = new InputStreamReader(getResources().getAssets().open(fileName));
+            BufferedReader bufReader = new BufferedReader(inputReader);
+            String line = "";
+            String Result = "";
+            while ((line = bufReader.readLine()) != null)
+                Result += line;
+            return Result;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
 }
