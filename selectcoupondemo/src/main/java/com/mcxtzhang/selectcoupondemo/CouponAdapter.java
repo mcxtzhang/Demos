78aad2f0c51c64141120923f001cb845964650db
zhangxutong <zhangxutong@imcoming.cn>
2016-09-29 12:45:21 +0800

    最普通写法初始化

---
+++ selectcoupondemo/src/main/java/com/mcxtzhang/selectcoupondemo/CouponAdapter.java
@@ -0,0 +1,67 @@
+package com.mcxtzhang.selectcoupondemo;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * 介绍：
+ * 作者：zhangxutong
+ * 邮箱：zhangxutong@imcoming.com
+ * 时间： 2016/9/29.
+ */
+
+public class CouponAdapter extends RecyclerView.Adapter<CouponAdapter.CouponVH> {
+    private List<TestBean> mDatas;
+    private Context mContext;
+    private LayoutInflater mInflater;
+
+    public CouponAdapter(List<TestBean> datas, Context context) {
+        mDatas = datas;
+        mContext = context;
+        mInflater = LayoutInflater.from(mContext);
+    }
+
+    @Override
+    public CouponVH onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new CouponVH(mInflater.inflate(R.layout.item_coupon, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(final CouponVH holder, int position) {
+        holder.ivSelect.setSelected(mDatas.get(position).isSelected());
+        holder.tvCoupon.setText(mDatas.get(position).getName());
+        holder.ivSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                for (TestBean data : mDatas) {
+                    data.setSelected(false);
+                }
+                mDatas.get(holder.getAdapterPosition()).setSelected(true);
+                notifyDataSetChanged();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return null != mDatas ? mDatas.size() : 0;
+    }
+
+    public static class CouponVH extends RecyclerView.ViewHolder {
+        private ImageView ivSelect;
+        private TextView tvCoupon;
+
+        public CouponVH(View itemView) {
+            super(itemView);
+            ivSelect = (ImageView) itemView.findViewById(R.id.ivSelect);
+            tvCoupon = (TextView) itemView.findViewById(R.id.tvCoupon);
+        }
+    }
+}
