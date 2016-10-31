be37a41b6ad218a4716a7855113f48ce6ca15aa0
zhangxutong <zhangxutong@imcoming.cn>
2016-10-31 11:39:48 +0800

    分类定义一个内部类 ，子类继承它，利用get set方法实现 堕胎

--- lib/src/main/java/com/example/Test.java
+++ lib/src/main/java/com/example/Test.java
@@ -14,13 +14,40 @@ class A {
     public void visit() {
         System.out.println(this.a);
         System.out.println(this.getClass());
+        getPresenter().output();
+    }
+
+    protected Presenter mPresenter;
+
+    public void setPresenter(Presenter presenter) {
+        mPresenter = presenter;
+    }
+
+    protected Presenter getPresenter() {
+        return mPresenter;
+    }
+
+    public class Presenter{
+        public void output(){
+            System.out.println("A:output");
+        }
     }
 }
 
 class B extends A {
+    public B() {
+        super();
+        setPresenter(new Presenter());
+    }
+
     private String a = "b";
 
     public void bb() {
         visit();
+    }
+    public class Presenter extends A.Presenter{
+        public void output(){
+            System.out.println("B:output");
+        }
     }
 }
