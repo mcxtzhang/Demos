8e278617d915cbdf44512c57407dcb4c31f2a70b
zhangxutong <zhangxutong@imcoming.cn>
2017-01-04 18:14:05 +0800

    【ViewPager 透明 Demo】

---
+++ viewpagerdemo/src/main/java/com/mcxtzhang/viewpagerdemo/MainActivity.java
@@ -0,0 +1,22 @@
+package com.mcxtzhang.viewpagerdemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.click).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, DetailActivity.class));
+            }
+        });
+    }
+}
