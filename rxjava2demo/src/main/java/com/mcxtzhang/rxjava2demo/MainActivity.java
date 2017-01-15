12c86bc24ebdfc71d4b2aa43186804ce2649a14c
zhangxutong <zhangxutong@imcoming.cn>
2017-01-16 00:19:47 +0800

    【AaptDemo】unfinished

--- rxjava2demo/src/main/java/com/mcxtzhang/rxjava2demo/MainActivity.java
+++ rxjava2demo/src/main/java/com/mcxtzhang/rxjava2demo/MainActivity.java
@@ -3,12 +3,15 @@ package com.mcxtzhang.rxjava2demo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 import com.mcxtzhang.rxjava2demo.retrofit.RetrofitDemoActivity;
 import com.mcxtzhang.rxjava2demo.rxjava2.Rx2Activity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "zxt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +30,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, RetrofitDemoActivity.class));
             }
         });
+        Log.d(TAG, "onCreate() called with: savedInstanceState = [" + getWindow() + "]");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called + getWindow()"+ getWindow());
     }
 }
