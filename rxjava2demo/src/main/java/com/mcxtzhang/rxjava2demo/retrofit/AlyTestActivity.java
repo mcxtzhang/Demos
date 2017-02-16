c8d55e7c954bfab5fa5f5e88011dc047b74e7c92
mcxtzhang <mcxtzhang@163.com>
2017-02-16 23:58:48 +0800

    【Retrofit2  Demo 】移动包结构

--- rxjava2demo/src/main/java/com/mcxtzhang/rxjava2demo/retrofit/AlyTestActivity.java
+++ rxjava2demo/src/main/java/com/mcxtzhang/rxjava2demo/retrofit/AlyTestActivity.java
@@ -52,6 +52,8 @@ public class AlyTestActivity extends AppCompatActivity {
         tvResult = (TextView) findViewById(R.id.tvResult);
         tvResult.setError("???");
 
+        Log.getStackTraceString(new IOException("dddd"));
+
         //header 追加统一参数
         Interceptor headerInterceptor = new Interceptor() {
             @Override
