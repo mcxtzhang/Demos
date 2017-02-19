0ef55926cbb56a9bcef51221bcd4adc68bdc7c4e
mcxtzhang <mcxtzhang@163.com>
2017-02-19 20:40:14 +0800

    【艺术探索 Demo 】1.2 启动模式 TaskAffinity 的测试

--- artdemo/src/main/java/anlaiye/com/cn/artdemo/c1/LifeCycleTest2Activity.java
+++ artdemo/src/main/java/anlaiye/com/cn/artdemo/c1/LifeCycleTest2Activity.java
@@ -1,9 +1,12 @@
 package anlaiye.com.cn.artdemo.c1;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.util.Log;
+import android.view.View;
 
 import anlaiye.com.cn.artdemo.R;
 
@@ -15,6 +18,21 @@ public class LifeCycleTest2Activity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_life_cycle_test2);
         Log.w(TAG, "onCreate() called with: savedInstanceState = [" + savedInstanceState + "]");
+        findViewById(R.id.btnReStart).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LifeCycleTest2Activity.this, LifeCycleTest2Activity.class));
+                Intent intent = new Intent();
+                intent.setDataAndType(Uri.parse("file://abc"),"image/png");
+            }
+        });
+
+        findViewById(R.id.btnJump1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LifeCycleTest2Activity.this, LifeCycleTestActivity.class));
+            }
+        });
     }
 
     @Override
@@ -69,5 +87,11 @@ public class LifeCycleTest2Activity extends Activity {
     protected void onDestroy() {
         super.onDestroy();
         Log.w(TAG, "onDestroy() called");
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Log.d(TAG, "onNewIntent() called with: intent = [" + intent + "]");
     }
 }
