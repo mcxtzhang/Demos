0503217a9f4a1eb9c1d965ffd0f56c07aaeb88e8
mcxtzhang <mcxtzhang@163.com>
2017-03-04 13:52:41 +0800

    【Dagger2 Demo】必须要有一个Component

---
+++ daggerdemo/src/main/java/com/mcxtzhang/daggerdemo/Main2Activity.java
@@ -0,0 +1,42 @@
+package com.mcxtzhang.daggerdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.mcxtzhang.daggerdemo.chap2.Entity2;
+
+import javax.inject.Inject;
+
+public class Main2Activity extends AppCompatActivity {
+
+    TextView tvCoffee;
+    @Inject
+    Entity2 mEntity2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        initView();
+    }
+
+    private void initView() {
+        tvCoffee = (TextView) findViewById(R.id.tv);
+        tvCoffee.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                makeCoffee();
+            }
+        });
+    }
+
+    private String makeCoffee() {
+        return mEntity2.fukc();
+    }
+
+}
---
+++ daggerdemo/src/main/java/com/mcxtzhang/daggerdemo/chap2/Entity2.java
@@ -0,0 +1,18 @@
+package com.mcxtzhang.daggerdemo.chap2;
+
+import javax.inject.Inject;
+
+/**
+ * Created by mcxtzhang on 2017/3/4.
+ */
+
+public class Entity2{
+
+    @Inject
+    public Entity2() {
+    }
+
+    public String fukc(){
+        return "ddd";
+    }
+}
