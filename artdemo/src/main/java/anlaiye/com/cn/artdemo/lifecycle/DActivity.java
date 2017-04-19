57d93fa5d6e0e3a69dc80fb7deacd59dbcbf708e
mcxtzhang <mcxtzhang@163.com>
2017-04-19 22:58:51 +0800

    【App LifeCycle Demo 】如果A是singleTop 则不会销毁 跳过onDestroy-onCreate,回回调onNewIntent

--- artdemo/src/main/java/anlaiye/com/cn/artdemo/lifecycle/DActivity.java
+++ artdemo/src/main/java/anlaiye/com/cn/artdemo/lifecycle/DActivity.java
@@ -19,6 +19,7 @@ public class DActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //A-B-C-D，依次启动，从D利用clearTop 回到A，会先执行 B C 的onDestroy()，然后D的onPause() ->A 会先销毁自己，onDestroy()->onCreate()-onStart().... 所以不会出现闪屏。但是A会销毁自己一次
+                //如果A是singleTop 则不会销毁 跳过onDestroy-onCreate,回回调onNewIntent
                 Intent intent = new Intent(DActivity.this, AActivity.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(intent);
