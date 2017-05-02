c2be681d0893c26d90d8c66a642cfcd9d38b34d7
mcxtzhang <mcxtzhang@163.com>
2017-05-02 15:01:47 +0800

    【Rxjava Demo】RxlifeCycle和takeUtil分析

--- rxjava2demo/src/main/java/com/mcxtzhang/rxjava2demo/lifecycle/CstRxLifecycleActivity.java
+++ rxjava2demo/src/main/java/com/mcxtzhang/rxjava2demo/lifecycle/CstRxLifecycleActivity.java
@@ -13,32 +13,41 @@ import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.ObservableSource;
 import io.reactivex.ObservableTransformer;
 import io.reactivex.Observer;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Predicate;
+import io.reactivex.observers.DisposableObserver;
 import io.reactivex.schedulers.Schedulers;
 
 public class CstRxLifecycleActivity extends AppCompatActivity {
 
     private static final String TAG = "zxt/RxLifeCycle";
 
+    CompositeDisposable mCompositeDisposable;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cst_rx_lifecycle);
 
+        mCompositeDisposable = new CompositeDisposable();
+
         final Observable<String> observable = Observable.create(new ObservableOnSubscribe<String>() {
             @Override
             public void subscribe(ObservableEmitter<String> e) throws Exception {
-                Log.e(TAG, "subscribe: 就算被取消了 也是会执行的呀" );
+                Log.e(TAG, "subscribe: 就算被取消了 也是会执行的吗？");
                 try {
                     Thread.sleep(3000);
                 } catch (InterruptedException e3) {
                     e3.printStackTrace();
                 }
-                e.onNext("1");
-                e.onNext("2");
-                Log.e(TAG, "subscribe: 就算被取消了 也是会执行的呀" );
-                e.onComplete();
+                if (!e.isDisposed()) {
+                    e.onNext("1");
+                    e.onNext("2");
+                    Log.e(TAG, "subscribe:  也是会执行的呀");
+                    e.onComplete();
+                }
+
             }
         }).subscribeOn(Schedulers.io());
 
@@ -135,6 +144,52 @@ public class CstRxLifecycleActivity extends AppCompatActivity {
                         });
             }
         });
+
+
+        findViewById(R.id.btnDispodeTest).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DisposableObserver<String> disposableObserver = observable.subscribeOn(Schedulers.io())
+                        .observeOn(Schedulers.computation())
+                        .compose(new ObservableTransformer<String, String>() {
+                            @Override
+                            public ObservableSource<String> apply(Observable<String> upstream) {
+                                return upstream.takeUntil(new Predicate<String>() {
+                                    @Override
+                                    public boolean test(String s) throws Exception {
+                                        return true;
+                                    }
+                                });
+                            }
+                        })
+                        .observeOn(Schedulers.io())
+                        .subscribeWith(new DisposableObserver<String>() {
+                            @Override
+                            public void onNext(String value) {
+                                Log.d(TAG, "onNext() called with: value = [" + value + "]");
+                            }
+
+                            @Override
+                            public void onError(Throwable e) {
+                                Log.d(TAG, "onError() called with: e = [" + e + "]");
+                            }
+
+                            @Override
+                            public void onComplete() {
+                                Log.d(TAG, "onComplete() called");
+                            }
+                        });
+
+                mCompositeDisposable.add(disposableObserver);
+                findViewById(R.id.btnDispode).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        mCompositeDisposable.dispose();
+                    }
+                });
+            }
+        });
+
 
     }
 
