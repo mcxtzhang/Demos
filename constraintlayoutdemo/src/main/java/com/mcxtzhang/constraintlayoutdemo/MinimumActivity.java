787f3da7cb2b6044d545c52d08bb0e3fe8a195a6
mcxtzhang <mcxtzhang@163.com>
2017-05-24 17:02:22 +0800

    【ConstraintLayout Demo】Ratio

---
+++ constraintlayoutdemo/src/main/java/com/mcxtzhang/constraintlayoutdemo/MinimumActivity.java
@@ -0,0 +1,13 @@
+package com.mcxtzhang.constraintlayoutdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MinimumActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_minimum);
+    }
+}
---
+++ constraintlayoutdemo/src/main/java/com/mcxtzhang/constraintlayoutdemo/WidgetsDimensionActivity.java
@@ -0,0 +1,13 @@
+package com.mcxtzhang.constraintlayoutdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WidgetsDimensionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_widgets_dimension);
+    }
+}
