6b9a0a64ad096eadff3c7f390238f446a48df906
mcxtzhang <mcxtzhang@163.com>
2017-09-17 12:27:49 +0800

    rotate drawable

--- performancedemo/src/main/java/anlaiye/com/cn/performancedemo/MainActivity.java
+++ performancedemo/src/main/java/anlaiye/com/cn/performancedemo/MainActivity.java
@@ -2,12 +2,15 @@ package anlaiye.com.cn.performancedemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 /**
  * 测试  ● Allocation Tracker：追踪内存对象的来源。
  */
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "zxt/Performance";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +32,15 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.testAllocationTrackerSb).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                StringBuilder sb  = new StringBuilder("temp");
+                StringBuilder sb = new StringBuilder("temp");
                 for (int i = 0; i < 1000; i++) {
                     sb.append(i);
                 }
 
             }
         });
+
+
+        Log.d(TAG, "onCreate() called with: savedInstanceState = [" + findViewById(R.id.root).getBackground().getClass() + "]");
     }
 }
