6d74fab5d814f4ed700b713980582c8d93a2bc61
mcxtzhang <mcxtzhang@163.com>
2017-10-26 17:28:41 +0800

    性能监控

--- performancedemo/src/main/java/anlaiye/com/cn/performancedemo/monitor/MonitorActivity.java
+++ performancedemo/src/main/java/anlaiye/com/cn/performancedemo/monitor/MonitorActivity.java
@@ -1,11 +1,11 @@
 package anlaiye.com.cn.performancedemo.monitor;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Choreographer;
+import android.support.v7.app.AppCompatActivity;
 
 import anlaiye.com.cn.performancedemo.R;
+
+import static anlaiye.com.cn.performancedemo.monitor.PerformanceMonitorUtils.monitorChoreoGrapher;
 
 public class MonitorActivity extends AppCompatActivity {
     private static final String TAG = "MonitorActivity";
@@ -15,22 +15,7 @@ public class MonitorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monitor);
 
-        Choreographer.getInstance().postFrameCallback(new Choreographer.FrameCallback() {
-            long lastTime;
-
-            @Override
-            public void doFrame(long frameTimeNanos) {
-                long gap = ((frameTimeNanos - lastTime) / 1000000);
-                Log.d(TAG, "doFrame() called with: frameTimeNanos = [" + frameTimeNanos + "]" + "lasttime:" + lastTime + ", gap:" + gap);
-                if (gap>16){
-                    long count = (gap-16) / 16 ;
-                    Log.e(TAG, "doFrame()丢帧 called with: frameTimeNanos = [" + frameTimeNanos + "]" + "lasttime:" + lastTime + ", gap:" + gap+",丢了几帧:"+count);
-
-                }
-                lastTime = frameTimeNanos;
-                Choreographer.getInstance().postFrameCallback(this);
-            }
-        });
+        monitorChoreoGrapher();
 
     }
 }
