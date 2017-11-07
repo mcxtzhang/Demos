f5f329c8226ed54051541b080f8b01c051f2800d
mcxtzhang <mcxtzhang@163.com>
2017-11-07 17:45:45 +0800

    测试 线程优先级

--- app/src/main/java/com/mcxtzhang/github/TestNewASActivity.java
+++ app/src/main/java/com/mcxtzhang/github/TestNewASActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Process;
 import android.support.v7.app.AppCompatActivity;
 import android.util.ArrayMap;
 import android.util.Log;
@@ -36,6 +37,8 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Vector;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 
 @DIActivity
 @TestHelloWorld("haha")
@@ -396,6 +399,33 @@ public class TestNewASActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         }, 3000);
+
+
+        //new PriorityThread().start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Log.d(TAG, "Thread.currentThread().getPriority() = [" + Thread.currentThread().getPriority() + "]");
+                Log.d(TAG, "Process.getThreadPriority() = [" + Process.getThreadPriority((int) Thread.currentThread().getId()) + "]");
+                Process.setThreadPriority(Process.THREAD_PRIORITY_FOREGROUND);
+                Log.d(TAG, "222 Process.getThreadPriority() = [" + Process.getThreadPriority((int) Thread.currentThread().getId()) + "]");
+                Log.d(TAG, "run() called:"+this);
+            }
+        }).start();
+
+    }
+
+    private static class PriorityThread extends Thread{
+
+        public PriorityThread() {
+
+        }
+
+        @Override
+        public void run() {
+            super.run();
+
+        }
     }
 
 
