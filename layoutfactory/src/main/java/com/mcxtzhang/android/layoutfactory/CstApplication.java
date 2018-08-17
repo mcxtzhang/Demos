f248fbcc46b529ef46c469d0e6d7040253180a70
mcxtzhang <mcxtzhang@163.com>
2018-08-17 11:17:13 +0800

    layoutfactory:若Activity已设置过，可以通过反射清空

---
+++ layoutfactory/src/main/java/com/mcxtzhang/android/layoutfactory/CstApplication.java
@@ -0,0 +1,115 @@
+package com.mcxtzhang.android.layoutfactory;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.view.LayoutInflaterCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.lang.reflect.Field;
+
+/**
+ * Created by zhangxutong on 2018/8/17.
+ */
+
+public class CstApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(final Activity activity, Bundle savedInstanceState) {
+                try {
+                    setFactory(activity);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    try {
+                        LayoutInflater layoutInflater = LayoutInflater.from(activity);
+
+                        Field mFactorySet = LayoutInflater.class.getDeclaredField("mFactorySet");
+                        mFactorySet.setAccessible(true);
+                        mFactorySet.set(layoutInflater, false);
+
+
+                        Field mFactory = LayoutInflater.class.getDeclaredField("mFactory");
+                        mFactory.setAccessible(true);
+                        mFactory.set(layoutInflater, null);
+                        setFactory(activity);
+                    } catch (Exception e2) {
+                        e2.printStackTrace();
+                    }
+                }
+
+            }
+
+            private void setFactory(final Activity activity) {
+                LayoutInflater layoutInflater = LayoutInflater.from(activity);
+                LayoutInflaterCompat.setFactory2(layoutInflater, new LayoutInflater.Factory2() {
+                    @Override
+                    public View onCreateView(String name, Context context, AttributeSet attrs) {
+                        return onCreateView(null, name, context, attrs);
+                    }
+
+                    @Override
+                    public View onCreateView(View parent, String name, Context context, AttributeSet attrs) {
+                        //先将构建View的行为仍交由AppCompatActivity的LayoutFactory去完成
+                        AppCompatDelegate delegate = ((AppCompatActivity) activity).getDelegate();
+                        View view = delegate.createView(parent, name, context, attrs);
+                        //在EditText被inflate出来时（其实已经是“AppCompatEditText”了），设置光标颜色值
+                        if (view instanceof EditText) {
+                            EditText editText = (EditText) view;
+                            try {
+                                Field f = TextView.class.getDeclaredField("mCursorDrawableRes");
+                                f.setAccessible(true);
+                                f.set(editText, R.drawable.cursor_color_bg_2);
+                            } catch (Exception e) {
+                            }
+                            return editText;
+                        }
+                        return view;
+                    }
+                });
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+
+            }
+        });
+    }
+}
--- layoutfactory/src/main/java/com/mcxtzhang/android/layoutfactory/MainActivity.java
+++ layoutfactory/src/main/java/com/mcxtzhang/android/layoutfactory/MainActivity.java
@@ -1,13 +1,89 @@
 package com.mcxtzhang.android.layoutfactory;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //在基类Activity#onCreate中抢先设置LayoutFactory
+//        LayoutInflaterCompat.setFactory2(LayoutInflater.from(this), new LayoutInflater.Factory2() {
+//            @Override
+//            public View onCreateView(String name, Context context, AttributeSet attrs) {
+//                return onCreateView(null, name, context, attrs);
+//            }
+//
+//            @Override
+//            public View onCreateView(View parent, String name, Context context, AttributeSet attrs) {
+//                //先将构建View的行为仍交由AppCompatActivity的LayoutFactory去完成
+//                AppCompatDelegate delegate = getDelegate();
+//                View view = delegate.createView(parent, name, context, attrs);
+//                //在EditText被inflate出来时（其实已经是“AppCompatEditText”了），设置光标颜色值
+//                if (view instanceof EditText) {
+//                    EditText editText = (EditText) view;
+//                    try {
+//                        java.lang.reflect.Field f = TextView.class.getDeclaredField("mCursorDrawableRes");
+//                        f.setAccessible(true);
+//                        f.set(editText, R.drawable.cursor_color_bg_2);
+//                    } catch (Exception e) {
+//                    }
+//                    return editText;
+//                }
+//                return view;
+//            }
+//        });
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LinearLayout linearLayout = findViewById(R.id.root);
+        linearLayout.addView(new EditText(this));
+
+//        final View decorView = getWindow().getDecorView();
+//        if (decorView instanceof ViewGroup) {
+//            final ViewGroup root = (ViewGroup) decorView;
+//            decorView.post(new Runnable() {
+//                @Override
+//                public void run() {
+//                    travesalSetCursorDrawable(root);
+//                }
+//            });
+//        }
+
+/*
+        EditText editText = findViewById(R.id.editText);
+        try {
+            java.lang.reflect.Field f = TextView.class.getDeclaredField("mCursorDrawableRes");
+            f.setAccessible(true);
+            f.set(editText, R.drawable.cursor_color_bg_2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+*/
+
     }
+
+//    public static void travesalSetCursorDrawable(ViewGroup parent) {
+//        int childCount = parent.getChildCount();
+//        for (int i = 0; i < childCount; i++) {
+//            View child = parent.getChildAt(i);
+//            if (child instanceof ViewGroup) {
+//                //recursion
+//                travesalSetCursorDrawable((ViewGroup) child);
+//            } else {
+//                if (child instanceof EditText) {
+//                    EditText editText = (EditText) child;
+//                    try {
+//                        java.lang.reflect.Field f = TextView.class.getDeclaredField("mCursorDrawableRes");
+//                        f.setAccessible(true);
+//                        f.set(editText, R.drawable.cursor_color_bg_2);
+//                    } catch (Exception e) {
+//                    }
+//                }
+//            }
+//        }
+//    }
 }
