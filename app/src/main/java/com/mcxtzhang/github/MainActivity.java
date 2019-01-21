dc5bb46563429984155395a403049cdcea4ae033
mcxtzhang <mcxtzhang@163.com>
2019-01-21 11:18:28 +0800

    demo edittext 加省略号

--- app/src/main/java/com/mcxtzhang/github/MainActivity.java
+++ app/src/main/java/com/mcxtzhang/github/MainActivity.java
@@ -1,39 +1,79 @@
 package com.mcxtzhang.github;
 
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
     LinearLayout ll;
+    private String storeString;
+    private String dotsString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ll = (LinearLayout) findViewById(R.id.rg);
+        final EditText et = (EditText) findViewById(R.id.content_title);
+        et.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    et.setText(storeString);
+                } else {
+                    String NOW = et.getText().toString();
+                    storeString = NOW;
 
-        final RadioButton rb1 = (RadioButton) findViewById(R.id.rb1);
-        RadioButton rb2 = (RadioButton) findViewById(R.id.rb2);
-        RadioButton rb3 = (RadioButton) findViewById(R.id.rb3);
-        RadioButton rb4 = (RadioButton) findViewById(R.id.rb4);
-        rb1.setOnClickListener(mOnClickListener);
-        rb2.setOnClickListener(mOnClickListener);
-        rb3.setOnClickListener(mOnClickListener);
-        rb4.setOnClickListener(mOnClickListener);
+
+                    //得到合适宽度：
+                    Rect indexBounds = new Rect();//存放每个绘制的index的Rect区域
+                    Paint paint = new Paint();
+                    //传入像素
+                    DisplayMetrics metrics = new DisplayMetrics();
+                    getWindowManager().getDefaultDisplay().getMetrics(metrics);
+                    paint.setTextSize(et.getTextSize());
+
+                    paint.getTextBounds(NOW, 0, NOW.length(), indexBounds);//测量计算文字所在矩形，可以得到宽高
+                    Log.d("TAG", "onCreate() called with: indexBounds.width() = [" + indexBounds.width() + "]");
+                    Log.d("TAG", "onCreate() called with: et.getTextSize() * NOW.length() = [" + et.getTextSize() * NOW.length() + "]");
+                    Log.d("TAG", "onCreate() called with: et.getWidth() = [" + et.getWidth() + "]");
+
+
+                    if (NOW != null && et.getWidth() < indexBounds.width()) {
+
+
+                        for (int i = 1; i < NOW.length(); i++) {
+                            paint.getTextBounds(NOW, 0, i, indexBounds);//测量计算文字所在矩形，可以得到宽高
+                            //if (indexBounds.width())
+                        }
+
+
+                        dotsString = NOW.substring(0, (int) (et.getWidth() / et.getTextSize())) + "...";
+
+                        et.setText(dotsString);
+
+                    }
+                }
+            }
+        });
+
 
     }
 
-    private View.OnClickListener mOnClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            if (!(v instanceof RadioButton)) return;
-            RadioButton rb = (RadioButton) v;
-            Log.d("TAG", "onClick() called with: v = [" + rb.isChecked() + "]");
-
+    public static CharSequence ellipsize(Editable text, EditText editText, int maxLength) {
+        CharSequence charSeq = "";
+        if (!TextUtils.isEmpty(text)) {
+            charSeq = TextUtils.ellipsize(text, editText.getPaint(), maxLength, TextUtils.TruncateAt.END);
+            charSeq = TextUtils.replace(charSeq, new String[]{"\u2026"}, new String[]{"..."});
         }
-    };
+        return charSeq;
+    }
 }
