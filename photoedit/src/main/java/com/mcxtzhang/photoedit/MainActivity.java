736f8eb5b0be67137bc52c68eae0f647487190d4
mcxtzhang <mcxtzhang@163.com>
2019-01-21 15:31:25 +0800

    demo : 图片旋转

---
+++ photoedit/src/main/java/com/mcxtzhang/photoedit/MainActivity.java
@@ -0,0 +1,52 @@
+package com.mcxtzhang.photoedit;
+
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    Bitmap bitmap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ImageView imageView = findViewById(R.id.image);
+
+        findViewById(R.id.image).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BitmapDrawable bitmapDrawable = (BitmapDrawable) imageView.getDrawable();
+                bitmap = bitmapDrawable.getBitmap();
+
+
+                int imageViewWidth = imageView.getWidth();
+                int imageViewHeight = imageView.getHeight();
+                int width = bitmap.getWidth();
+                int height = bitmap.getHeight();
+                float scaleWidth = ((float) imageViewWidth) / width;
+                float scaleHeight = ((float) imageViewHeight) / height;
+                Matrix imageMatrix = imageView.getImageMatrix();
+                imageMatrix.postScale(scaleWidth, scaleHeight);
+                imageView.setImageMatrix(imageMatrix);
+            }
+        });
+
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BitmapDrawable bitmapDrawable = (BitmapDrawable) imageView.getDrawable();
+                bitmap = bitmapDrawable.getBitmap();
+
+                bitmap = PhotoEditUtils.rotaingImageView(90, bitmap);
+                imageView.setImageBitmap(bitmap);
+            }
+        });
+    }
+}
---
+++ photoedit/src/main/java/com/mcxtzhang/photoedit/PhotoEditUtils.java
@@ -0,0 +1,127 @@
+package com.mcxtzhang.photoedit;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+
+import java.io.InputStream;
+
+/**
+ * Created by zhangxutong on 2019/1/21.
+ */
+
+public class PhotoEditUtils {
+    /**
+     * 通过资源id转化成Bitmap
+     *
+     * @param context
+     * @param resId
+     * @return
+     */
+    public static Bitmap readBitmapById(Context context, int resId) {
+        BitmapFactory.Options opt = new BitmapFactory.Options();
+        opt.inPreferredConfig = Bitmap.Config.RGB_565;
+        opt.inPurgeable = true;
+        opt.inInputShareable = true;
+        InputStream is = context.getResources().openRawResource(resId);
+        return BitmapFactory.decodeStream(is, null, opt);
+    }
+
+    /**
+     * 缩放图片
+     *
+     * @param bm        要缩放图片
+     * @param newWidth  宽度
+     * @param newHeight 高度
+     * @return处理后的图片
+     */
+    public static Bitmap scaleImage(Bitmap bm, int newWidth, int newHeight) {
+        if (bm == null) {
+            return null;
+        }
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        Matrix matrix = new Matrix();
+        matrix.postScale(scaleWidth, scaleHeight);
+        Bitmap newbm = Bitmap.createBitmap(bm, 0, 0, width, height, matrix, true);
+        if (bm != null & !bm.isRecycled()) {
+            bm.recycle();//销毁原图片
+            bm = null;
+        }
+        return newbm;
+    }
+
+    /**
+     * 按照一定的宽高比例裁剪图片
+     *
+     * @param bitmap     要裁剪的图片
+     * @param num1       长边的比例
+     * @param num2       短边的比例
+     * @param isRecycled 是否回收原图片
+     * @return 裁剪后的图片
+     */
+    public static Bitmap imageCrop(Bitmap bitmap, int num1, int num2, boolean isRecycled) {
+        if (bitmap == null) {
+            return null;
+        }
+        int w = bitmap.getWidth(); // 得到图片的宽，高
+        int h = bitmap.getHeight();
+        int retX, retY;
+        int nw, nh;
+        if (w > h) {
+            if (h > w * num2 / num1) {
+                nw = w;
+                nh = w * num2 / num1;
+                retX = 0;
+                retY = (h - nh) / 2;
+            } else {
+                nw = h * num1 / num2;
+                nh = h;
+                retX = (w - nw) / 2;
+                retY = 0;
+            }
+        } else {
+            if (w > h * num2 / num1) {
+                nh = h;
+                nw = h * num2 / num1;
+                retY = 0;
+                retX = (w - nw) / 2;
+            } else {
+                nh = w * num1 / num2;
+                nw = w;
+                retY = (h - nh) / 2;
+                retX = 0;
+            }
+        }
+        Bitmap bmp = Bitmap.createBitmap(bitmap, retX, retY, nw, nh, null, false);
+        if (isRecycled && bitmap != null && !bitmap.equals(bmp) && !bitmap.isRecycled()) {
+            bitmap.recycle();//回收原图片
+            bitmap = null;
+        }
+        return bmp;
+    }
+
+    /**
+     * 旋转图片
+     * @param angle 旋转角度
+     * @param bitmap 要处理的Bitmap
+     * @return 处理后的Bitmap
+     */
+    public static Bitmap rotaingImageView(int angle, Bitmap bitmap)
+    {
+        // 旋转图片 动作
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        // 创建新的图片
+        Bitmap resizedBitmap = Bitmap.createBitmap(bitmap, 0, 0,
+                bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+        if (resizedBitmap != bitmap && bitmap != null && !bitmap.isRecycled()){
+            bitmap.recycle();
+            bitmap = null;
+        }
+        return resizedBitmap;
+    }
+}
