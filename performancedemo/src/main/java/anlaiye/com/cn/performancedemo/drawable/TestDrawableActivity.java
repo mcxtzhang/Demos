ff31a3b5c960c1dc5f3eadc6cc355b1a286050e2
mcxtzhang <mcxtzhang@163.com>
2020-06-24 10:58:17 +0800

    Feature：验证Drawable放在drawable/目录下 等于drawable-nodpi

---
+++ performancedemo/src/main/java/anlaiye/com/cn/performancedemo/drawable/TestDrawableActivity.java
@@ -0,0 +1,15 @@
+package anlaiye.com.cn.performancedemo.drawable;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import anlaiye.com.cn.performancedemo.R;
+
+public class TestDrawableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_drawable_1);
+    }
+}
