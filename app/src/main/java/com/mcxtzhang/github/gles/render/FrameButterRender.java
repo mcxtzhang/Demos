956cb26aecd664bad3137bd3f2d304e7bc996910
mcxtzhang <mcxtzhang@163.com>
2020-07-30 14:25:02 +0800

    Feature： opengl demo 8  帧缓存FrameBuffer，利用2个FrameBuffer，对图片做三次处理

--- app/src/main/java/com/mcxtzhang/github/gles/render/FrameButterRender.java
+++ app/src/main/java/com/mcxtzhang/github/gles/render/FrameButterRender.java
@@ -50,6 +50,16 @@ public class FrameButterRender implements GLSurfaceView.Renderer {
                     "    gl_FragColor = color;\n" +
                     "}\n";
 
+    private String fragmentShaderCode3 =
+            "precision mediump float;\n" +
+                    "varying vec2 v_textureCoordinate;\n" +
+                    "uniform sampler2D u_texture;\n" +
+                    "void main() {\n" +
+                    "    vec4 color = texture2D(u_texture, v_textureCoordinate);\n" +
+                    "    color.r = 0.3;\n" +
+                    "    gl_FragColor = color;\n" +
+                    "}\n";
+
 
     // GLSurfaceView的宽高
     // The width and height of GLSurfaceView
@@ -73,22 +83,28 @@ public class FrameButterRender implements GLSurfaceView.Renderer {
     // The texture coordinate
     private float[] textureCoordinateData0 = new float[]{0f, 1f, 0f, 0f, 1f, 0f, 0f, 1f, 1f, 0f, 1f, 1f};
     private float[] textureCoordinateData1 = new float[]{0f, 0f, 0f, 1f, 1f, 1f, 0f, 0f, 1f, 1f, 1f, 0f};
+    private float[] textureCoordinateData2 = new float[]{0f, 0f, 0f, 1f, 1f, 1f, 0f, 0f, 1f, 1f, 1f, 0f};
+
     private FloatBuffer textureCoordinateDataBuffer0;
     private FloatBuffer textureCoordinateDataBuffer1;
+    private FloatBuffer textureCoordinateDataBuffer2;
     private int TEXTURE_COORDINATE_COMPONENT_COUNT = 2;
 
     // 2个GL Program
     // two GL Programs
     private int programId1 = 0;
     private int programId2 = 0;
+    private int programId3 = 0;
 
     // 帧缓存
     // frame buffer
     private int frameBuffer = 0;
+    private int frameBuffer3 = 0;
 
     // 帧缓绑定的texture
     // the texture bind on the frame buffer
     private int frameBufferTexture = 0;
+    private int frameBufferTexture3 = 0;
 
     // 图片texture
     // image texture
@@ -110,6 +126,7 @@ public class FrameButterRender implements GLSurfaceView.Renderer {
         // 创建2个GL Program，第一个用来做均值模糊，第二做普通纹理贴图
         programId1 = createGLProgram(vertexShaderCode, fragmentShaderCode1);
         programId2 = createGLProgram(vertexShaderCode, fragmentShaderCode2);
+        programId3 = createGLProgram(vertexShaderCode, fragmentShaderCode3);
 
 
     }
@@ -143,6 +160,22 @@ public class FrameButterRender implements GLSurfaceView.Renderer {
         bindGLProgram(programId2, frameBufferTexture, textureCoordinateDataBuffer1);
 
         // 绑定0号frame buffer
+        bindFrameBuffer(frameBuffer3);
+
+        // 执行渲染，渲染效果水平方向的模糊
+        render();
+
+
+        // 绑定第1个GL Program
+        bindGLProgram(programId3, frameBufferTexture3, textureCoordinateDataBuffer2);
+
+        // 绑定0号frame buffer
+        /**
+         * 这里注意一点，0号frame buffer是一个特殊的frame buffer，它是默认的frame buffer，
+         * 即如果我们没有使用glBindFramebuffer()去绑定过frame buffer，它就是绑定到0号frame buffer上的，
+         * 0号frame buffer通常代表屏幕，离屏渲染除外，这个暂不讨论，现在大家只需要知道将frame buffer绑定到0就能渲染到屏幕上就行了。
+         *
+         */
         bindFrameBuffer(0);
 
         // 执行渲染，渲染效果水平方向的模糊
@@ -213,6 +246,12 @@ public class FrameButterRender implements GLSurfaceView.Renderer {
         textureCoordinateDataBuffer1.put(textureCoordinateData1)
                 .position(0);
 
+        textureCoordinateDataBuffer2 = ByteBuffer.allocateDirect(textureCoordinateData2.length * java.lang.Float.SIZE)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer();
+        textureCoordinateDataBuffer2.put(textureCoordinateData2)
+                .position(0);
+
         int[] textures = new int[1];
         GLES20.glGenTextures(textures.length, textures, 0);
         imageTexture = textures[0];
@@ -256,6 +295,26 @@ public class FrameButterRender implements GLSurfaceView.Renderer {
         GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, frameBuffer);
         GLES20.glFramebufferTexture2D(GLES20.GL_FRAMEBUFFER, GLES20.GL_COLOR_ATTACHMENT0, GLES20.GL_TEXTURE_2D, frameBufferTexture, 0);
         //GLES20.glTexImage2D(GLES20.GL_TEXTURE_2D, 0, GLES20.GL_RGBA, width, height, 0, GLES20.GL_RGBA, GLES20.GL_UNSIGNED_BYTE, null);
+
+
+        int[] textures3 = new int[1];
+        GLES20.glGenTextures(1, textures3, 0);
+        frameBufferTexture3 = textures3[0];
+
+        int[] frameBuffers3 = new int[1];
+        GLES20.glGenFramebuffers(1, frameBuffers3, 0);
+        frameBuffer3 = frameBuffers3[0];
+
+        //然后将texture与frame buffer的color attachment绑定：
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, frameBufferTexture3);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE);
+        GLES20.glTexImage2D(GLES20.GL_TEXTURE_2D, 0, GLES20.GL_RGBA, width, height, 0, GLES20.GL_RGBA, GLES20.GL_UNSIGNED_BYTE, null);
+
+        GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, frameBuffer3);
+        GLES20.glFramebufferTexture2D(GLES20.GL_FRAMEBUFFER, GLES20.GL_COLOR_ATTACHMENT0, GLES20.GL_TEXTURE_2D, frameBufferTexture3, 0);
 
     }
 
