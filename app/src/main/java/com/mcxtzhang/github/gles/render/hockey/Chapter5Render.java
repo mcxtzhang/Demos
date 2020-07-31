db37c9bd7ffd6fd0ab82d100d0ccbf40c92171df
mcxtzhang <mcxtzhang@163.com>
2020-07-31 17:26:41 +0800

    Feature： Chapter 5 finish,学习了线性代数基础，理解矩阵Matrix 和向量相乘的结果；   利用正交投影矩阵，修复横竖屏坐标系绝对宽高不同的问题

--- app/src/main/java/com/mcxtzhang/github/gles/render/hockey/Chapter5Render.java
+++ app/src/main/java/com/mcxtzhang/github/gles/render/hockey/Chapter5Render.java
@@ -2,6 +2,7 @@ package com.mcxtzhang.github.gles.render.hockey;
 
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
@@ -16,12 +17,13 @@ public class Chapter5Render implements GLSurfaceView.Renderer {
     //varying是一个特殊的变量类型，它把给它的那些值进行混合，并把这些混合后的值发送给片段着色器；
     private String vertexShaderCode =
             "precision mediump float;\n" +
+                    "uniform mat4 u_Matrix;\n" +
                     "attribute vec4 a_Position;\n" +
                     "attribute vec4 aa_Color;\n" +
                     "varying vec4 v_Color;\n" +
                     "void main() {\n" +
                     "    v_Color = aa_Color;\n" +
-                    "    gl_Position = a_Position;\n" +
+                    "    gl_Position = u_Matrix * a_Position;\n" +
                     "    gl_PointSize = 10.0;\n" +
 
                     "}";
@@ -44,11 +46,11 @@ public class Chapter5Render implements GLSurfaceView.Renderer {
 
             //Triangle Fan
             0, 0, 1f, 1f, 1f,
-            -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
-            0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
-            0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
-            -0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
-            -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+            -0.25f, -0.5f, 0.7f, 0.7f, 0.7f,
+            0.25f, -0.5f, 0.7f, 0.7f, 0.7f,
+            0.25f, 0.5f, 0.7f, 0.7f, 0.7f,
+            -0.25f, 0.5f, 0.7f, 0.7f, 0.7f,
+            -0.25f, -0.5f, 0.7f, 0.7f, 0.7f,
 
             //line 1
             -0.5f, 0f, 1f, 0f, 0f,
@@ -65,8 +67,12 @@ public class Chapter5Render implements GLSurfaceView.Renderer {
     private FloatBuffer vertexDataBuffer;
 
 
+    private final float[] projectionMatrix = new float[16];
+
+
     private int aPositionLocation;
     private int aaColorLocation;
+    private int uMatrixLocation;
 
     public Chapter5Render() {
         vertexDataBuffer = ByteBuffer.allocateDirect(vertexAttrArray.length * Float.SIZE)
@@ -95,10 +101,25 @@ public class Chapter5Render implements GLSurfaceView.Renderer {
         GLES20.glVertexAttribPointer(aaColorLocation, COLOR_COMPONENT_COUNT, GLES20.GL_FLOAT, false, STRIDE, vertexDataBuffer);
         GLES20.glEnableVertexAttribArray(aaColorLocation);
 
+
+        uMatrixLocation = GLES20.glGetUniformLocation(programId, "u_Matrix");
+
     }
 
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
+        GLES20.glViewport(0, 0, width, height);
+
+        final float aspectRatio = width > height ?
+                (float) (width) / height :
+                (float) height / width;
+        if ((width > height)) {
+            //横屏
+            Matrix.orthoM(projectionMatrix, 0, -aspectRatio, aspectRatio, -1, 1, -1, 1);
+        } else {
+            Matrix.orthoM(projectionMatrix, 0, -1, 1, -aspectRatio, aspectRatio, -1, 1);
+        }
+
 
     }
 
@@ -107,6 +128,8 @@ public class Chapter5Render implements GLSurfaceView.Renderer {
         GLES20.glClearColor(0f, 0, 0f, 0f);
         GLES20.glClear((GLES20.GL_COLOR_BUFFER_BIT));
 
+
+        GLES20.glUniformMatrix4fv(uMatrixLocation, 1, false, projectionMatrix, 0);
 
         GLES20.glDrawArrays(GLES20.GL_TRIANGLE_FAN, 0, 6);
 
